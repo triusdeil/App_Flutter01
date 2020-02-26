f6a718bd19e28694ec5c747ba9c3030585bede79
Triusdeil <34854558+triusdeil@users.noreply.github.com>
2020-02-26 20:29:44 +0100

    Aplicacion concluida
    Front-end listo se cambio el tamaño los contenedores para poder mostrar las letras y demas contenido

--- app_clima/lib/main.dart
+++ app_clima/lib/main.dart
@@ -27,20 +27,67 @@ class MyApp extends StatelessWidget {
 class  HomePage extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-      return  Column(
-        children:<Widget>[
-          dayWidget("Lunes",Icons.wb_cloudy,"45 °C"),
-          dayWidget("Martes",Icons.wb_sunny,"40 °C"),
-          dayWidget("Miercoles",Icons.wb_cloudy,"35 °C"),
-          dayWidget("Jueves",Icons.wb_sunny,"45 °C"),
-          dayWidget("Viernes",Icons.wb_cloudy,"40 °C")
-        ]
+      return Column(
+      children:<Widget>[
+        header(),
+          list()
+         ],
+         );
+    }
+    Widget header(){
+      return Stack(
+          children: <Widget>[
+            Container(
+              height: 100.0, 
+              width: double.infinity,
+            child: Image.network("https://i.picsum.photos/id/1018/3914/2935.jpg",
+            fit: BoxFit.cover,
+            ),
+            ),
+            Container(
+              height: 100.0, 
+              width: double.infinity,
+              color: Colors.black38,
+            ),
+            Positioned(
+              left: 16.0,
+              top: 36.0,
+              child: Text("Mordor(Guafal)", 
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold)
+                ,),
+      ),
+      Positioned(
+        right: 8.0,
+        bottom: 8.0,
+        child: Text("32C", style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold)),
+      ),
+            ],
       );
     }
+
+Widget list(){
+  return Padding
+      (padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children:<Widget>[
+          dayWidget("lunes",Icons.wb_cloudy,"30 °C"),
+          dayWidget("Martes",Icons.wb_cloudy,"35 °C"),
+          dayWidget("Miercoles",Icons.wb_sunny,"40 °C"),
+          dayWidget("Jueves",Icons.wb_cloudy,"30 °C"),
+          dayWidget("Viernes",Icons.wb_sunny,"40 °C")
+        ]
+      ),
+      );
+}
+
     Widget dayWidget(String day, IconData iconData, String temp){
       return Card(
         child: Padding( 
-          padding: const EdgeInsets.all(24.0),
+          padding: const EdgeInsets.all(16.0),
          child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children:<Widget>[
